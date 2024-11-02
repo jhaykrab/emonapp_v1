790f1636a318effcce439408b4181a2caac84360
jhaykrab <jakecasundo15@gmail.com>
2024-11-02 13:36:04 +0800

    fixed  device_info_widget.dart

--- lib/widgets/device_info_widget.dart
+++ lib/widgets/device_info_widget.dart
@@ -56,8 +56,8 @@ class _DeviceInfoWidgetState extends State<DeviceInfoWidget>
     return Center(
       child: Container(
         width: 450,
-        padding: EdgeInsets.all(16.0),
-        margin: EdgeInsets.only(top: 40.0), // Increased top margin
+        padding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 0.0),
+        margin: EdgeInsets.only(top: 40.0),
         decoration: BoxDecoration(
           color: const Color.fromARGB(255, 223, 236, 219),
           border: Border.all(
@@ -82,197 +82,137 @@ class _DeviceInfoWidgetState extends State<DeviceInfoWidget>
               child: Text(
                 DateFormat('MMMM d, yyyy - EEEE').format(DateTime.now()),
                 style: TextStyle(
-                  fontSize: 18,
+                  fontSize: 16,
                   fontWeight: FontWeight.bold,
                   color: const Color.fromARGB(255, 72, 100, 68),
                 ),
               ),
             ),
-            SizedBox(height: 16),
+            SizedBox(height: 12),
 
-            // Labels Row
+            // Row for Appliance Icon/Toggle and Readings/Units/Values
             Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text(
-                  'kWh',
-                  style: TextStyle(
-                    fontSize: 16,
-                    color: const Color.fromARGB(255, 72, 100, 68),
+                // Column for Appliance Icon and Toggle
+                Column(
+                  children: [],
+                ),
+
+                SizedBox(width: 16), // Spacing between icon/toggle and readings
+
+                // Column for Readings, Units, and Values
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      // Readings Titles Row
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          SizedBox(width: 20),
+                          _buildReadingTitle('Energy'),
+                          SizedBox(width: 15),
+                          _buildReadingTitle('Voltage'),
+                          SizedBox(width: 15),
+                          _buildReadingTitle('Current'),
+                          SizedBox(width: 15),
+                          _buildReadingTitle('Power'),
+                          SizedBox(width: 15),
+                          _buildReadingTitle('Runtime'),
+                          SizedBox(width: 40),
+                        ],
+                      ),
+
+                      SizedBox(height: 4), // Reduced spacing
+
+                      // Units Row
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          SizedBox(width: 80),
+                          _buildUnit('kWh'),
+                          SizedBox(width: 40),
+                          _buildUnit('V'),
+                          SizedBox(width: 40),
+                          _buildUnit('A'),
+                          SizedBox(width: 40),
+                          _buildUnit('W'),
+                          SizedBox(width: 40),
+                          _buildUnit('h:m:s'),
+                          SizedBox(width: 100),
+                        ],
+                      ),
+
+                      SizedBox(height: 8), // Reduced spacing
+
+                      // Values Row
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(bottom: 8.0),
+                            child: Icon(
+                              Icons.lightbulb,
+                              size: 36, // Reduced icon size
+                              color: const Color.fromARGB(255, 72, 100, 68),
+                            ),
+                          ),
+                          _buildReadingValue(widget.energy.toStringAsFixed(2)),
+                          _buildReadingValue(widget.voltage.toStringAsFixed(1)),
+                          _buildReadingValue(widget.current.toStringAsFixed(2)),
+                          _buildReadingValue(widget.power.toStringAsFixed(1)),
+                          _buildReadingValue(
+                              '${widget.runtimehr}:${widget.runtimemin}:${widget.runtimesec}'),
+                          Transform.scale(
+                            scale: 0.8,
+                            child: Switch(
+                              value: widget.isApplianceOn,
+                              onChanged: widget.onToggleChanged,
+                              activeTrackColor: Colors.green[700],
+                              activeColor: Colors.green[900],
+                              inactiveTrackColor: Colors.grey[400],
+                              inactiveThumbColor: Colors.grey[300],
+                            ),
+                          ),
+                          // Delete Icon (conditionally visible with animation)
+                          if (_showDeleteIcon)
+                            AnimatedBuilder(
+                              animation: _animationController,
+                              builder: (context, child) {
+                                return Transform.rotate(
+                                  angle: _animationController.value *
+                                      2 *
+                                      math.pi /
+                                      12,
+                                  child: child,
+                                );
+                              },
+                              child: IconButton(
+                                icon: Icon(Icons.delete, color: Colors.red),
+                                onPressed: () {
+                                  _showDeleteConfirmationDialog();
+                                },
+                              ),
+                            ),
+                        ],
+                      ),
+                    ],
                   ),
                 ),
-                SizedBox(width: 43),
-                SizedBox(width: 1),
-                Text(
-                  'V',
-                  style: TextStyle(
-                    fontSize: 16,
-                    color: const Color.fromARGB(255, 72, 100, 68),
-                  ),
-                ),
-                SizedBox(width: 30),
-                SizedBox(width: 8),
-                Text(
-                  'A',
-                  style: TextStyle(
-                    fontSize: 16,
-                    color: const Color.fromARGB(255, 72, 100, 68),
-                  ),
-                ),
-                SizedBox(width: 15),
-                SizedBox(width: 20),
-                Text(
-                  'W',
-                  style: TextStyle(
-                    fontSize: 16,
-                    color: const Color.fromARGB(255, 72, 100, 68),
-                  ),
-                ),
-                SizedBox(width: 1),
-                SizedBox(width: 25),
-                Text(
-                  'Runtime',
-                  style: TextStyle(
-                    fontSize: 14,
-                    color: const Color.fromARGB(255, 72, 100, 68),
-                  ),
-                ),
-                SizedBox(width: 10),
               ],
             ),
-            SizedBox(height: 4), // Spacing between labels and values
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Icon(
-                  Icons.lightbulb, // Replace with your device icon
-                  size: 40,
-                  color: const Color.fromARGB(255, 72, 100, 68),
-                ),
-                SizedBox(width: 20),
-                Text(
-                  '${widget.energy.toStringAsFixed(2)}',
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                    color: const Color.fromARGB(255, 72, 100, 68),
-                  ),
-                ),
-                SizedBox(width: 10),
-                Text(
-                  '${widget.voltage.toStringAsFixed(1)}',
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                    color: const Color.fromARGB(255, 72, 100, 68),
-                  ),
-                ),
-                SizedBox(width: 10), // Spacing between voltage and current
-                Text(
-                  '${widget.current.toStringAsFixed(2)}',
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                    color: const Color.fromARGB(255, 72, 100, 68),
-                  ),
-                ),
-                SizedBox(width: 10), // Spacing between current and power
-                Text(
-                  '${widget.power.toStringAsFixed(1)}',
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                    color: const Color.fromARGB(255, 72, 100, 68),
-                  ),
-                ),
-                SizedBox(width: 10), // Spacing between power and runtime
-                Text(
-                  '${widget.runtimehr}h ${widget.runtimemin}m ${widget.runtimesec}s', // Display runtime from database
-                  style: TextStyle(
-                    fontSize: 14,
-                    fontWeight: FontWeight.bold,
-                    color: const Color.fromARGB(255, 72, 100, 68),
-                  ),
-                ),
-                SizedBox(width: 10),
-                Switch(
-                  value: widget.isApplianceOn,
-                  onChanged: widget.onToggleChanged,
-                  activeTrackColor: Colors.green[700],
-                  activeColor: Colors.green[900],
-                  inactiveTrackColor: Colors.grey[400],
-                  inactiveThumbColor: Colors.grey[300],
-                ),
-                // Delete Icon (conditionally visible with animation)
-                if (_showDeleteIcon)
-                  AnimatedBuilder(
-                    animation: _animationController,
-                    builder: (context, child) {
-                      return Transform.rotate(
-                        angle: _animationController.value * 2 * math.pi / 12,
-                        child: child,
-                      );
-                    },
-                    child: IconButton(
-                      icon: Icon(Icons.delete, color: Colors.red),
-                      onPressed: () {
-                        showDialog(
-                          context: context,
-                          builder: (BuildContext context) {
-                            return AlertDialog(
-                              title: Text('Delete Device'),
-                              content: Text(
-                                  'Are you sure you want to delete this device?'),
-                              actions: [
-                                TextButton(
-                                  onPressed: () {
-                                    setState(() {
-                                      _showDeleteIcon =
-                                          false; // Hide the delete icon
-                                    });
-                                    Navigator.of(context)
-                                        .pop(); // Close the dialog
-                                    _animationController
-                                        .reset(); // Stop and reset the animation
-                                  },
-                                  child: Text(
-                                    'Cancel',
-                                    style: TextStyle(
-                                      color: Colors.red,
-                                    ),
-                                  ),
-                                ),
-                                TextButton(
-                                  onPressed: () {
-                                    // TODO: Implement device deletion logic here
-                                    // Remove the device from your data source
-                                    // Update the UI to reflect the deletion
-                                    Navigator.of(context)
-                                        .pop(); // Close the dialog
-                                  },
-                                  child: Text('Confirm',
-                                      style: TextStyle(
-                                        color: const Color.fromARGB(
-                                            255, 54, 83, 56),
-                                      )),
-                                ),
-                              ],
-                            );
-                          },
-                        );
-                      },
-                    ),
-                  ),
-              ],
-            ),
-            SizedBox(height: 24), // Increased spacing
+
+            SizedBox(height: 24),
+
             // Add and Remove Device Buttons
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                ElevatedButton(
-                  onPressed: () {
-                    // Navigate to DeviceConfigurationScreen
+                _buildElevatedButton(
+                  'Add a Device',
+                  const Color.fromARGB(255, 54, 83, 56),
+                  () {
                     Navigator.push(
                       context,
                       MaterialPageRoute(
@@ -280,24 +220,11 @@ class _DeviceInfoWidgetState extends State<DeviceInfoWidget>
                       ),
                     );
                   },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: const Color.fromARGB(255, 54, 83, 56),
-                    minimumSize: Size(180, 40), // Increased width to 180
-                    padding: EdgeInsets.symmetric(horizontal: 35, vertical: 12),
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(5)),
-                  ),
-                  child: Text(
-                    'Add a Device',
-                    style: TextStyle(
-                      fontSize: 12,
-                      fontWeight: FontWeight.normal,
-                      color: Colors.white,
-                    ),
-                  ),
                 ),
-                ElevatedButton(
-                  onPressed: () {
+                _buildElevatedButton(
+                  'Remove a Device',
+                  const Color.fromARGB(255, 202, 67, 67),
+                  () {
                     setState(() {
                       _showDeleteIcon = !_showDeleteIcon;
                       if (_showDeleteIcon) {
@@ -307,27 +234,120 @@ class _DeviceInfoWidgetState extends State<DeviceInfoWidget>
                       }
                     });
                   },
-                  style: ElevatedButton.styleFrom(
-                    minimumSize: Size(180, 40), // Increased width to 180
-                    backgroundColor: const Color.fromARGB(255, 202, 67, 67),
-                    padding: EdgeInsets.symmetric(horizontal: 35, vertical: 12),
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(5)),
-                  ),
-                  child: Text(
-                    'Remove a Device',
-                    style: TextStyle(
-                      fontSize: 12,
-                      fontWeight: FontWeight.normal,
-                      color: Colors.white, // Changed font color to white
-                    ),
-                  ),
                 ),
               ],
             ),
           ],
         ),
       ),
+    );
+  }
+
+  // Helper function to build reading titles
+  Widget _buildReadingTitle(String title) {
+    return Text(
+      title,
+      style: TextStyle(
+        fontWeight: FontWeight.bold,
+        fontSize: 12,
+        color: const Color.fromARGB(255, 72, 100, 68),
+      ),
+    );
+  }
+
+  // Helper function to build reading values
+  Widget _buildReadingValue(String value) {
+    return Text(
+      value,
+      style: TextStyle(
+        fontSize: 12,
+        fontWeight: FontWeight.bold,
+        color: const Color.fromARGB(255, 72, 100, 68),
+      ),
+    );
+  }
+
+  // Helper function to build unit labels
+  Widget _buildUnit(String unit) {
+    return Text(
+      unit,
+      style: TextStyle(
+        fontSize: 10,
+        color: const Color.fromARGB(255, 72, 100, 68),
+      ),
+    );
+  }
+
+  // Helper function to build elevated buttons
+  Widget _buildElevatedButton(
+      String label, Color color, VoidCallback onPressed) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      style: ElevatedButton.styleFrom(
+        backgroundColor: color,
+        minimumSize: Size(160, 40),
+        padding: EdgeInsets.symmetric(horizontal: 24, vertical: 10),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
+      ),
+      child: Text(
+        label,
+        style: TextStyle(
+          fontSize: 12,
+          fontWeight: FontWeight.normal,
+          color: Colors.white,
+        ),
+      ),
+    );
+  }
+
+  // Function to show a confirmation dialog before removing a device
+  Future<void> _showDeleteConfirmationDialog() async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(
+            'Delete Device',
+            style: TextStyle(color: Color.fromARGB(255, 54, 83, 56)),
+          ),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                Text(
+                  'Are you sure you want to delete this device?',
+                  style: TextStyle(color: Color.fromARGB(255, 22, 22, 22)),
+                ),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: Text(
+                'Cancel',
+                style: TextStyle(color: Color.fromARGB(255, 54, 83, 56)),
+              ),
+              onPressed: () {
+                setState(() {
+                  _showDeleteIcon = false;
+                });
+                Navigator.of(context).pop();
+                _animationController.reset();
+              },
+            ),
+            TextButton(
+              child: Text(
+                'Confirm',
+                style: TextStyle(color: Color.fromARGB(255, 114, 18, 18)),
+              ),
+              onPressed: () {
+                // TODO: Implement device deletion logic here
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
     );
   }
 }
